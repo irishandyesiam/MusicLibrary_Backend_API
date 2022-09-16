6bd562b3340cc55de0eb8f6cf15856beb39df591
irishandyesiam <nathan.irish@gmail.com>
2022-09-16 15:27:13 -0500

    Registered ML_B Model with Admin site + completed POST Functionalityfor ML_B.

--- musiclibrary_backend/views.py
+++ musiclibrary_backend/views.py
@@ -1,9 +1,22 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SongSerializer
+from .models import Song
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 
 def musiclibrary_list(request):
 
+    if request.method == 'GET':
+        musiclibrary_songs = Song.objects.all()
+        serializer = SongSerializer(musiclibrary_songs, many=True)
+        return Response(serializer.data)
 
-    return Response('ok')
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
